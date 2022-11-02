fdef8ed56f8abe609854b28210348a2ca7bc8221
GGG <rhlanf7gh53@gmail.com>
2022-11-02 09:54:45 +0900

    테스트중입니다

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,6 @@
 
-^
-.src/main/java/com/project/Controller/Test.java,c\d\cd9ac0d48d82658aac5021cd1abbb46f3700b88a
+:
+
+.classpath,3\5\354079a72b91a4280407c16a36f47d1986fd85a5
+8
+.project,f\8\f8b41ce76d7b80d2b47e91b5f92c15f00eccf187
---
+++ src/main/java/com/project/menus/dao/MenuDao.java
@@ -0,0 +1,21 @@
+package com.project.menus.dao;
+
+import java.util.List;
+
+import com.green.menus.vo.MenuVo;
+
+public interface MenuDao {
+
+	List<MenuVo> getMenuList();
+
+	void insertMenu(MenuVo menuVo);
+
+	void insertMenu2(MenuVo menuVo);
+
+	void deleteMenu(String menu_id);
+
+	MenuVo menuView(String menu_id);
+
+	void menuUpdate(MenuVo menuVo);
+	
+}
